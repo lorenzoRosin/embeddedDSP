20c280a9d2cfb7594170c6c430e8bed9c504d0fa
LORENZODELL\rosin <rosin.lorenzo@hotmail.it>
2023-12-03 21:21:28 +0100

    update

--- C/Src/Filter/Src/eDSP_FOLPASSFILTER.c
+++ C/Src/Filter/Src/eDSP_FOLPASSFILTER.c
@@ -39,7 +39,7 @@ e_eDSP_FOLPASSFILTER_RES eDSP_FOLPASSFILTER_InitCtx(t_eDSP_FOLPASSFILTER_Ctx* co
 	else
 	{
 		/* Check data validity */
-		if( p_uWindowsBuffLen <= 2u )
+		if( 0u == p_uCutFreqMilHz )
 		{
 			l_eRes = e_eDSP_FOLPASSFILTER_RES_BADPARAM;
 		}
@@ -47,10 +47,8 @@ e_eDSP_FOLPASSFILTER_RES eDSP_FOLPASSFILTER_InitCtx(t_eDSP_FOLPASSFILTER_Ctx* co
 		{
 			/* Initialize internal status */
 			p_ptCtx->bIsInit = true;
-			p_ptCtx->uWindowsLen = p_uWindowsBuffLen;
-			p_ptCtx->uFilledData = 0u;
+			p_ptCtx->uCutFreqMilHz = p_uCutFreqMilHz;
 			p_ptCtx->iPrevOutValue = 0u;
-			memset(p_piWindowsBuffer, 0, sizeof(int64_t));
 
 			/* All OK */
 			l_eRes = e_eDSP_FOLPASSFILTER_RES_OK;
@@ -95,26 +93,6 @@ e_eDSP_FOLPASSFILTER_RES eDSP_FOLPASSFILTER_InsertValueAndCalculate(t_eDSP_FOLPA
 	int64_t l_iNearestDiff;
 	int64_t l_iCurrDiff;
 
-
-	/* To calculate the First order Low pass filter we will use an RC circuit.
-
-	   -----/\/\/\----------
-	   				  |
-		Vin			 ___    Vout
-					 ___
-					  |
-	   ---------------------
-	   The equantion of the circuit is:
-	   Vin(ti)-Vout(ti) = Vr(ti) -> Vin(ti)-Vout(ti) = R * Ir(ti) -> Vin(ti)-Vout(ti) = R * ( C * (dVout(ti)/dT) ) ->
-	   and using discrete time we have: Vin(ti)-Vout(ti) = RC * ( ( Vout(i)-Vout(i-1) ) / ( t(i)-t(i-1) ) ) ) ->
-	   Vout(ti) = Vin(ti) - RC * ( ( Vout(i)-Vout(i-1) ) / ( t(i)-t(i-1) ) ) ) -> ..... ->
-	   Vout(ti) = ( ( t(i)-t(i-1) ) / ( RC+( t(i)-t(i-1) )  ) ) * Vin(ti) + ( RC / ( RC + ( t(i)-t(i-1) ))) * Vout(ti-1)
-	   Doing other math in the frequency domains we found out that the cutoff frequency is Fc = 1 / ( 2 pi RC )
-	   and so the value of RC = 1 / ( 2 pi Fc )
-	*/
-
-
-
 	/* Check pointer validity */
 	if( ( NULL == p_ptCtx ) || ( NULL == p_pFilteredVal ) )
 	{
@@ -136,94 +114,30 @@ e_eDSP_FOLPASSFILTER_RES eDSP_FOLPASSFILTER_InsertValueAndCalculate(t_eDSP_FOLPA
             }
 			else
 			{
-				/* Insert data */
-				p_ptCtx->piWindowsBuffer[p_ptCtx->iPrevOutValue] = p_iValue;
-				p_ptCtx->iPrevOutValue++;
+				/* To calculate the First order Low pass filter we will use an RC circuit.
 
-				/* Manage rolback */
-				if( p_ptCtx->iPrevOutValue >= p_ptCtx->uWindowsLen )
-				{
-					p_ptCtx->iPrevOutValue = 0u;
-				}
+					-----/\/\/\----------
+									|
+						Vin		   ___    Vout
+								   ___
+									|
+					---------------------
+					The equantion of the circuit is:
+					Vin(ti) - Vout(ti) = Vr(ti)
+					Vin(ti) - Vout(ti) = R * Ir(ti)
+					Vin(ti) - Vout(ti) = R *  ( C * ( dVout(ti) / dT ) )
+					Vin(ti) - Vout(ti) = RC * ( dVout(ti) / dT )
+					and using discrete time we have:
+					Vin(ti) - Vout(ti) = RC * (    ( Vout(i) - Vout(i-1) )    /    ( t(i) - t(i-1) )    )
+					Vout(ti) = Vin(ti) - RC * (    ( Vout(i) - Vout(i-1) )    /    ( t(i) - t(i-1) )    )
+					... More calc ...
+					Vout(ti) = ( ( t(i)-t(i-1) ) / ( RC + ( t(i)-t(i-1) ) ) * Vin(ti) +
+							   ( RC / ( RC + ( t(i)-t(i-1) ) )              * Vout(ti-1)
+					Doing other math in the frequency domains we found out that the cutoff frequency is
+					Fc = 1 / ( 2 pi RC ) and so the value of RC = 1 / ( 2 pi Fc )
+				*/
 
-				if( p_ptCtx->uFilledData < ( p_ptCtx->uWindowsLen - 1u ) )
-				{
-					/* Increase filler counter */
-					p_ptCtx->uFilledData++;
-
-					/* Need more data */
-					l_eRes = e_eDSP_FOLPASSFILTER_RES_NEEDSMOREVALUE;
-				}
-				else
-				{
-					/* The window is full */
-					l_eRes = e_eDSP_FOLPASSFILTER_RES_OK;
-					l_uCnt = 0u;
-					l_iSum = 0u;
-
-					/* Calculate the factibility of the sum for the means */
-					while( ( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes ) && ( l_uCnt < p_ptCtx->uWindowsLen ) )
-					{
-						l_eMaxRes = eDSP_MAXCHECK_SUMI64Check(l_iSum, p_ptCtx->piWindowsBuffer[l_uCnt]);
-						l_eRes = eDSP_FOLPASSFILTER_MaxCheckResToFOLPF(l_eMaxRes);
-
-						if( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes )
-						{
-							l_uCnt++;
-							l_iSum += p_ptCtx->piWindowsBuffer[l_uCnt];
-						}
-					}
-
-					if( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes )
-					{
-						/* Calculate the mean */
-						l_iMean = l_iSum / p_ptCtx->uWindowsLen;
-
-						/* re-init counter */
-						l_uCnt = 0u;
-
-						/* search for the nearest one */
-						while( ( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes ) && ( l_uCnt < p_ptCtx->uWindowsLen ) )
-						{
-							l_eMaxRes = eDSP_MAXCHECK_SUBTI64Check(l_iMean, p_ptCtx->piWindowsBuffer[l_uCnt]);
-							l_eRes = eDSP_FOLPASSFILTER_MaxCheckResToFOLPF(l_eMaxRes);
-
-							if( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes )
-							{
-								/* Calc diff */
-								l_iCurrDiff = l_iMean - p_ptCtx->piWindowsBuffer[l_uCnt];
-
-								/* abs of the difference */
-								if( l_iCurrDiff < 0u )
-								{
-									l_iCurrDiff = -l_iCurrDiff;
-								}
-
-								/* On first round init default variable */
-								if( 0u == l_uCnt )
-								{
-									/* On first round init default variable */
-									l_iNearest = p_ptCtx->piWindowsBuffer[l_uCnt];
-									l_iNearestDiff = l_iCurrDiff;
-								}
-								else
-								{
-									/* Compare with the alredy founded */
-									if( l_iCurrDiff < l_iNearestDiff )
-									{
-										l_iNearestDiff = p_ptCtx->piWindowsBuffer[l_uCnt];
-									}
-								}
-							}
-						}
-
-						/* if all ok return value */
-						if( e_eDSP_FOLPASSFILTER_RES_OK == l_eRes )
-						{
-							*p_pFilteredVal = l_iNearest;
-						}
-					}
-				}
+				l_eRes = e_eDSP_FOLPASSFILTER_RES_OK;
 			}
 		}
     }
@@ -242,30 +156,13 @@ static bool_t eDSP_FOLPASSFILTER_IsStatusStillCoherent(t_eDSP_FOLPASSFILTER_Ctx*
     bool_t l_eRes;
 
 	/* Check pointer validity */
-	if( NULL == p_ptCtx->piWindowsBuffer )
+	if( 0u == p_ptCtx->uCutFreqMilHz )
 	{
 		l_eRes = false;
 	}
     else
     {
-		/* Check data validity */
-		if( ( p_ptCtx->uWindowsLen <= 2u ) || ( p_ptCtx->uFilledData > p_ptCtx->uWindowsLen ) ||
-			( p_ptCtx->iPrevOutValue >= p_ptCtx->uWindowsLen )  )
-		{
-			l_eRes = false;
-		}
-		else
-		{
-			/* Check data validity */
-			if( ( p_ptCtx->uFilledData < p_ptCtx->uWindowsLen ) && ( p_ptCtx->iPrevOutValue >= p_ptCtx->uFilledData ) )
-			{
-				l_eRes = false;
-			}
-			else
-			{
-				l_eRes = true;
-			}
-		}
+		l_eRes = true;
     }
 
     return l_eRes;
